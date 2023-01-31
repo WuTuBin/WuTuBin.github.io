91f560ad0594e598f43009f75d79ee216ccfabf5
wutubin <807941247@qq.com>
2023-01-31 09:40:07 +0800

    chore: test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home-container page-container">
     <img class="vue-element-plus-logo" alt="Vue logo" src="../assets/logo.png" />
-    <div class="page-title">Vite2.x + Vue3.x + TypeScript + Element Plus</div>
+    <div class="page-title">飞飞飞Vite2.x + Vue3.x + TypeScript + Element Plus</div>
   </div>
 </template>
 
